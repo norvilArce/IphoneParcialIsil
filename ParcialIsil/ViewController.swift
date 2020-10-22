4362bd02e353246fc427e8902543c5076de74216
Norvil <norvil.arce@gmail.com>
2020-10-22 12:35:00 -0700

    here i go again

---
+++ ParcialIsil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParcialIsil
+//
+//  Created by user178680 on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
